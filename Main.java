16eb21ec31ed7f9a0643baba46f7db9e49ea1a31
Bhargav <143892094+vexora-0@users.noreply.github.com>
2024-07-27 20:22:34 +0530

    updatdd main

--- Main.java
+++ Main.java
@@ -4,37 +4,66 @@ import com.example.controllers.GameController;
 import com.example.exceptions.InvalidMoveException;
 import com.example.models.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {public static void main(String[] args) throws InvalidMoveException {
-    System.out.println("Hello world!");
-    Scanner scanner = new Scanner(System.in);
-    GameController gameController = new GameController();
+public class Main {
+    public static void main(String[] args) throws InvalidMoveException {
+        System.out.println("Bhargav");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        GameController gameController = new GameController();
 
-    int dimension = 3;
-    List<Player> players = List.of(
-            new Player("Harsh", new Symbol('X'), PlayerType.HUMAN),
-            new Bot("Scaler", new Symbol('O'), PlayerType.BOT, BotDifficultyLevel.EASY)
-    );
+        int dimension = 3;
+        List<Player> players = List.of(
+                new Player("Bhargav", new Symbol('X'), PlayerType.HUMAN),
+            
+                new Bot("hackclub", new Symbol('O'), PlayerType.BOT, BotDifficultyLevel.EASY)
+        );
 
-    Game game = gameController.startGame(dimension, players);
-    //gameController.printBoard(game);
+        boolean playAgain;
+        do {
+            Game game = gameController.startGame(dimension, players);
+            List<String> moveLog = new ArrayList<>(); // Track moves
+            while (game.getGameState().equals(GameState.IN_PROGRESS)) {
 
-    while (game.getGameState().equals(GameState.IN_PROGRESS)) {
-        //1. print the board.
-        gameController.printBoard(game);
+                                gameController.printBoard(game);
 
-        //2. Player's turn
-        gameController.makeMove(game);
+                gameController.makeMove(game);
+
+
+                                moveLog.add(game.getLastMove().toString());
+            }
+
+            // Check the game state
+            if (!gameController.checkState(game).equals(GameState.ENDED)) {
+                game.setGameState(GameState.DRAW);
+                System.out.println("Game DRAW");
+            } else {
+                gameController.printBoard(game);
+                System.out.println("Player " + gameController.getWinner(game).getName() + " is the winner");
+            }
+
+            System.out.println("Move Log:");
+            for (String move : moveLog) {
+                System.out.println(move);
+            }
+
+             if (gameController.checkState(game).equals(GameState.ENDED)) {
+                game.setGameState(GameState.DRAW);
+                System.out.println("Game WON");
+            } else {
+                gameController.printBoard(game);
+                System.out.println("Player " + gameController.getWinner(game).getName() + " is the winner");
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            playAgain = scanner.next().equalsIgnoreCase("yes");
+
+        } while (playAgain);
+
+        System.out.println("Thank you for playing!");
     }
-
-    if (!gameController.checkState(game).equals(GameState.ENDED)) {
-        game.setGameState(GameState.DRAW);
-        System.out.println("Game DRAW");
-    } else {
-        gameController.printBoard(game);
-        System.out.println("Player " + gameController.getWinner(game).getName() + " is the winner");
-    }
-}
 }
